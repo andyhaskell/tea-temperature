f7f2fd1facb506f471ee8be1df228cc2b8662289
&y Haskell <andy.with.ampersand@gmail.com>
2020-04-19 23:35:56 -0400

    Add struct tags to JSON weather fields

--- climacell/weather.go
+++ climacell/weather.go
@@ -8,21 +8,23 @@ import (
 // whose type is a floating-point number. If Value is nil, that means that for
 // the field this FloatValue is for, data is not available.
 type FloatValue struct {
-	Value *float64
-	Units string
+	Value *float64 `json:"value"`
+	Units string   `json:"units"`
 }
 
 // NonNullableTimeValue is a field on a Weather for timestamps. This type is
 // used for fields that should always be present on a weather sample, such as
 // observation timestamps.
-type NonNullableTimeValue struct{ Value time.Time }
+type NonNullableTimeValue struct {
+	Value time.Time `json:"value"`
+}
 
 // Weather represents a weather sample from the ClimaCell API. Note that in the
 // full API, there are many more fields that can be present, but for this
 // tutorial we are only deserializing temperature.
 type Weather struct {
-	Lat             float64
-	Lon             float64
-	Temp            *FloatValue
-	ObservationTime NonNullableTimeValue
+	Lat             float64              `json:"lat"`
+	Lon             float64              `json:"lon"`
+	Temp            *FloatValue          `json:"temp"`
+	ObservationTime NonNullableTimeValue `json:"observation_time"`
 }
