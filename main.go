871bd95e7785d5d4a247786eb97f883440566ca6
&y Haskell <andy.with.ampersand@gmail.com>
2020-04-20 00:05:50 -0400

    Use ClimaCell HTTP client in main.go
    Now we know what kind of tea to make at 5PM!

--- main.go
+++ main.go
@@ -1,45 +1,39 @@
 package main
 
 import (
-	"encoding/json"
 	"log"
-	"net/http"
 	"os"
+	"time"
 
 	"github.com/andyhaskell/tea-temperature/climacell"
 )
 
 func main() {
-	req, err := http.NewRequest(
-		http.MethodGet,
-		"https://api.climacell.co/v3/weather/forecast/hourly?lat=42.3826&lon=-71.1460&fields=temp",
-		nil,
-	)
+	c := climacell.New(os.Getenv("CLIMACELL_API_KEY"))
+	weatherSamples, err := c.HourlyForecast(climacell.ForecastArgs{
+		LatLon:     &climacell.LatLon{Lat: 42.3826, Lon: -71.146},
+		UnitSystem: "us",
+		Fields:     []string{"temp"},
+		StartTime:  time.Now(),
+		EndTime:    time.Now().Add(24 * time.Hour),
+	})
 	if err != nil {
-		log.Fatalf("error creating HTTP request: %v", err)
+		log.Fatalf("error getting forecast data: %v", err)
 	}
 
-	req.Header.Add("Accept", "application/json")
-	req.Header.Add("apikey", os.Getenv("CLIMACELL_API_KEY"))
-
-	res, err := http.DefaultClient.Do(req)
-	if err != nil {
-		log.Fatalf("error sending HTTP request: %v", err)
-	}
-
-	var weatherSamples []climacell.Weather
-	d := json.NewDecoder(res.Body)
-	if err := d.Decode(&weatherSamples); err != nil {
-		log.Fatalf("error deserializing weather data")
-	}
-
-	for _, w := range weatherSamples {
-		if w.Temp != nil && w.Temp.Value != nil {
-			log.Printf("The temperature at %s is %f degrees %s\n",
-				w.ObservationTime.Value, *w.Temp.Value, w.Temp.Units)
-		} else {
-			log.Printf("No temperature data available at %s\n",
-				w.ObservationTime.Value)
+	var tempAtFive *climacell.FloatValue
+	for i, w := range weatherSamples {
+		if w.ObservationTime.Value.Hour() == 21 {
+			tempAtFive = weatherSamples[i].Temp
+			break
 		}
+	}
+
+	if tempAtFive == nil || tempAtFive.Value == nil {
+		log.Printf("No data on the temperature at 5, let's wing it! 🌺\n")
+	} else if t := *tempAtFive.Value; t < 60 {
+		log.Printf("It'll be %f out. Better make some hot tea! 🌺🍵\n", t)
+	} else {
+		log.Printf("It'll be %f out. Iced tea it is! 🌺🍹\n", t)
 	}
 }
