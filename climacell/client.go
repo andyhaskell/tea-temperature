bc8b8a116d33b25a20b3201de3efc566032cb394
&y Haskell <andy.with.ampersand@gmail.com>
2020-04-20 00:00:23 -0400

    Define hourly forecast method on ClimaCell client.
    For convenience, we add a ForecastArgs struct for constructing the
    query parameters we send to the hourly forecast endpoint. Next, we will
    use the client in main.go!

--- climacell/client.go
+++ climacell/client.go
@@ -1,6 +1,7 @@
 package climacell
 
 import (
+	"encoding/json"
 	"net/http"
 	"net/url"
 	"time"
@@ -26,4 +27,35 @@ func New(apiKey string) *Client {
 		c:      c,
 		apiKey: apiKey,
 	}
+}
+
+// HourlyForecast gets an hourly forecast for a location.
+func (c *Client) HourlyForecast(args ForecastArgs) ([]Weather, error) {
+	// set up a request to the hourly forecast endpoint
+	endpt := baseURL.ResolveReference(
+		&url.URL{Path: "weather/forecast/hourly"})
+	req, err := http.NewRequest("GET", endpt.String(), nil)
+	if err != nil {
+		return nil, err
+	}
+
+	// add URL headers, query params, then send the request
+	req.Header.Add("Accept", "application/json")
+	req.Header.Add("apikey", c.apiKey)
+	req.URL.RawQuery = args.QueryParams().Encode()
+
+	res, err := c.c.Do(req)
+	if err != nil {
+		return nil, err
+	}
+
+	// deserialize the response and return our weather data
+	defer res.Body.Close()
+	var weatherSamples []Weather
+	if err := json.NewDecoder(res.Body).Decode(&weatherSamples); err != nil {
+		return nil, err
+	}
+	return weatherSamples, nil
+
+	// [TODO] handle error responses
 }
--- climacell/weather.go
+++ climacell/weather.go
@@ -1,8 +1,77 @@
 package climacell
 
 import (
+	"net/url"
+	"strconv"
+	"strings"
 	"time"
 )
+
+//
+// request query parameters
+//
+
+// LatLon represents a pair of latitude and longitude coordinates.
+type LatLon struct{ Lat, Lon float64 }
+
+// ForecastArgs represents query parameters that can be sent to the ClimaCell
+// API when requesting weather data. We create query parameters with the
+// QueryParams method.
+type ForecastArgs struct {
+	// If present, latitude and longitude coordinates we are requesting
+	// forecast data for.
+	LatLon *LatLon
+	// If non-blank, ID for location we are requesting forecast data for.
+	LocationID string
+	// Unit system to return weather data in. Valid values are "si" and "us",
+	// default is "si"
+	UnitSystem string
+	// Weather data fields we want returned in the response
+	Fields []string
+	// If nonzero, StartTime indicates the initial timestamp to request weather
+	// data from.
+	StartTime time.Time
+	// If nonzero, EndTime indicates the ending timestamp to request weather
+	// data to.
+	EndTime time.Time
+}
+
+// QueryParams converts this ForecastArgs into a url.Values struct so it can be
+// used as query parameters for weather requests. Each field on the
+// ForecastArgs struct is only converted to a query parameter if that field is
+// not its zero value.
+func (args ForecastArgs) QueryParams() url.Values {
+	q := make(url.Values)
+
+	if args.LatLon != nil {
+		q.Add("lat", strconv.FormatFloat(args.LatLon.Lat, 'f', -1, 64))
+		q.Add("lon", strconv.FormatFloat(args.LatLon.Lon, 'f', -1, 64))
+	}
+
+	if args.LocationID != "" {
+		q.Add("location_id", args.LocationID)
+	}
+	if args.UnitSystem != "" {
+		q.Add("unit_system", args.UnitSystem)
+	}
+
+	if len(args.Fields) > 0 {
+		q.Add("fields", strings.Join(args.Fields, ","))
+	}
+
+	if !args.StartTime.IsZero() {
+		q.Add("start_time", args.StartTime.Format(time.RFC3339))
+	}
+	if !args.EndTime.IsZero() {
+		q.Add("end_time", args.EndTime.Format(time.RFC3339))
+	}
+
+	return q
+}
+
+//
+// response deserialization
+//
 
 // FloatValue is a field on a weather sample returned from the ClimaCell API
 // whose type is a floating-point number. If Value is nil, that means that for
